c4240011caea5df967ac6e19c2a3b201da8b1203
MrFranksJr <frankyjr@me.com>
2022-10-30 16:21:03 +0100

    update to description

--- index.js
+++ index.js
@@ -226,3 +226,15 @@ function initLikeButtons() {
     localStorage.clear()
     document.location.reload(true)
 }) */
+
+
+//darkmode
+const dModeToggle = document.getElementById("darkmode-toggle")
+const bodyEl = document.body
+console.log(dModeToggle)
+
+dModeToggle.addEventListener("change", e => {
+    if(e.target.checked){
+       console.log("checked")
+    }
+})
